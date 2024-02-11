302f24696011129c8f9a8211f526d0e924fa2477
jtdub <amascuba@gmail.com>
2024-02-11 14:23:25 -0600

    add filter test

--- nautobot_itential/tests/test_filter_automationgatewaymodel.py
+++ nautobot_itential/tests/test_filter_automationgatewaymodel.py
@@ -1,28 +1,28 @@
 """Test AutomationGatewayModel Filter."""
 
-# from django.test import TestCase
-# from nautobot_itential import filters
-# from nautobot_itential import models
-# from nautobot_itential.tests import fixtures
+from django.test import TestCase
+from nautobot_itential import filters
+from nautobot_itential import models
+from nautobot_itential.tests import fixtures
 
 
-# class AutomationGatewayModelFilterTestCase(TestCase):
-#     """AutomationGatewayModel Filter Test Case."""
-#
-#     queryset = models.AutomationGatewayModel.objects.all()
-#     filterset = filters.AutomationGatewayModelFilterSet
-#
-#     @classmethod
-#     def setUpTestData(cls):
-#         """Setup test data for AutomationGatewayModel Model."""
-#         fixtures.create_automationgatewaymodel()
-#
-#     def test_q_search_name(self):
-#         """Test using Q search with name of AutomationGatewayModel."""
-#         params = {"q": "NYC Gateway"}
-#         self.assertEqual(self.filterset(params, self.queryset).qs.count(), 1)
-#
-#     def test_q_invalid(self):
-#         """Test using invalid Q search for AutomationGatewayModel."""
-#         params = {"q": "test-five"}
-#         self.assertEqual(self.filterset(params, self.queryset).qs.count(), 0)
+class AutomationGatewayModelFilterTestCase(TestCase):
+    """AutomationGatewayModel Filter Test Case."""
+
+    queryset = models.AutomationGatewayModel.objects.all()
+    filterset = filters.AutomationGatewayModelFilterSet
+
+    @classmethod
+    def setUpTestData(cls):
+        """Setup test data for AutomationGatewayModel Model."""
+        fixtures.create_automationgatewaymodel()
+
+    def test_q_search_name(self):
+        """Test using Q search with name of AutomationGatewayModel."""
+        params = {"q": "NYC Gateway"}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 1)
+
+    def test_q_invalid(self):
+        """Test using invalid Q search for AutomationGatewayModel."""
+        params = {"q": "test-five"}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 0)
